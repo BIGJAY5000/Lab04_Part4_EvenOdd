3ec67d6f0a0d61ab6bbc7dccf7ce09621e5f9d29
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-09-19 09:33:08 -0400

    dookie credit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+
+    //variables
+        int num;
+
+        System.out.println("Enter a number please");
+        num = scan.nextInt();
+
+        if (num % 2 == 1)
+            System.out.println("your number is odd");
+        else
+            System.out.println("your number is even");
     }
 }
